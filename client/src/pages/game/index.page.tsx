11e3bd0556da76aabc0b42a4a2b45b58230dc51e
1f10230234 <s1f102302348@iniad.org>
2023-08-17 22:06:41 +0900

    add state for game.tsx

--- client/src/pages/game/index.page.tsx
+++ client/src/pages/game/index.page.tsx
@@ -1,6 +1,43 @@
+import { useEffect, useState } from 'react';
 import styles from './index.module.css';
+// import { apiClient } from 'src/utils/apiClient';
+export type pos = {
+  x: number;
+  y: number;
+};
 
 const Game = () => {
+  const [bombMap, setBombMap] = useState<(0 | 1)[][]>();
+  const [userInputs, setUserInputs] = useState<(0 | 1 | 2)[][]>();
+  const [board, setBoard] = useState<number[][]>();
+  const [openCells, setOpenCells] = useState<pos[]>([]);
+
+  const fetchGame = async () => {
+    // await apiClient.game.post(openCells)
+    // const res = await apiClient.game.get()
+    // if (res !== null) {
+    //   if (bombMap === null) setBombMap(res.bombMap);
+    //   setUserInputs(res.userInputs)
+    // }
+  };
+
+  const fetchBombMap = async () => {
+    // const res = await apiClient.game.start.g$et();
+    // if (res === null) fetchBombMap();
+    // setBombMap(res);
+  };
+
+  useEffect(() => {
+    const cancelId = setInterval(() => {
+      fetchGame();
+    }, 2000);
+    return () => clearInterval(cancelId);
+  }, []);
+
+  useEffect(() => {
+    fetchBombMap();
+  }, []);
+
   return <div className={styles.container}>{/*コンテンツ*/}</div>;
 };
 
