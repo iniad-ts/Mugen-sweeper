ac6c29dfea3c56c63b52c03123f82aa06e39b181
1f10230234 <s1f102302348@iniad.org>
2023-08-18 00:59:06 +0900

    fix template for front

--- client/src/pages/game/index.page.tsx
+++ client/src/pages/game/index.page.tsx
@@ -1,17 +1,41 @@
 import { useEffect, useState } from 'react';
+import { Loading } from 'src/components/Loading/Loading';
 import styles from './index.module.css';
 // import { apiClient } from 'src/utils/apiClient';
-export type pos = {
+export type Pos = {
   x: number;
   y: number;
 };
+export type boardMOdel = number[][];
+
+const directions = [
+  [0, 1],
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+];
 
 const Game = () => {
   const [bombMap, setBombMap] = useState<(0 | 1)[][]>();
   const [userInputs, setUserInputs] = useState<(0 | 1 | 2)[][]>();
-  const [board, setBoard] = useState<number[][]>();
-  const [openCells, setOpenCells] = useState<pos[]>([]);
-  const [lranking, setRanking] = useState();
+  const [ranking, setRanking] = useState();
+
+  const newBoard = bombMap?.map((row) => row.map(() => -1));
+
+  useEffect(() => {
+    const cancelId = setInterval(() => {
+      fetchGame();
+    }, 2000);
+    return () => clearInterval(cancelId);
+  }, []);
+
+  useEffect(() => {
+    fetchBombMap();
+  }, []);
 
   const fetchGame = async () => {
     // await apiClient.game.post(openCells)
@@ -29,16 +53,29 @@ const Game = () => {
     // setBombMap(res);
   };
 
-  useEffect(() => {
-    const cancelId = setInterval(() => {
-      fetchGame();
-    }, 2000);
-    return () => clearInterval(cancelId);
-  }, []);
+  if (newBoard === undefined || bombMap === undefined || userInputs === undefined) {
+    fetchGame();
+    return <Loading visible />;
+  }
+  const recursion = (x: number, y: number) => {
+    newBoard[y][x] =
+      bombMap
+        .slice(Math.max(0, y - 1), Math.min(y + 2, bombMap.length))
+        .map((row) => row.slice(Math.max(0, x - 1), Math.min(x + 2, row.length)))
+        .flat()
+        .filter((b) => b === 1).length ?? 1 - 1;
 
-  useEffect(() => {
-    fetchBombMap();
-  }, []);
+    if (newBoard[y][x] === 0) {
+      directions
+        .map((direction) => ({ x: x + direction[0], y: y + direction[1] }))
+        .filter((nextPos) => newBoard[nextPos.y][nextPos.x] === -1)
+        .forEach((nextPos) => {
+          recursion(nextPos.x, nextPos.y);
+        });
+    }
+  };
+
+  userInputs.forEach((row, y) => row.forEach((_, x) => recursion(x, y)));
 
   return (
     <div className={styles.container}>
