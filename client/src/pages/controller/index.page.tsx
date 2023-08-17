4b242fb779c8ab069d1c19396d04814e48158287
1f10230234 <s1f102302348@iniad.org>
2023-08-17 17:56:55 +0900

    add: frontのディレクトリ

---
+++ client/src/pages/controller/index.page.tsx
@@ -0,0 +1,13 @@
+import styles from './index.module.css';
+
+const Controller = () => {
+  return (
+    <div className={styles.controller}>
+      <div>{/*＋字キー*/}</div>
+      <div>{/*ディスプレイ*/}</div>
+      <div>{/*スコップ＋旗*/}</div>
+    </div>
+  );
+};
+
+export default Controller;
---
+++ client/src/pages/game/index.page.tsx
@@ -0,0 +1,7 @@
+import styles from './index.module.css';
+
+const Game = () => {
+  return <div className={styles.container}>{/*コンテンツ*/}</div>;
+};
+
+export default Game;
