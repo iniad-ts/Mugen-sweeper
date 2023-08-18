8b213c3981a8b4e5e03419adeb81fb7e65d31e1c
1f10230234 <s1f102302348@iniad.org>
2023-08-18 23:25:18 +0900

    fix lint

--- server/service/make2DArray.ts
+++ server/service/make2DArray.ts
@@ -1,2 +1,2 @@
-export const make2DArray = (width: number, height: number):0[][] =>
+export const make2DArray = (width: number, height: number): 0[][] =>
   [...Array(height)].map(() => [...Array(width)].map(() => 0));
