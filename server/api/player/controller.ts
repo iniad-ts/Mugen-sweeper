1f0de8304f037b6f8796f447dbef36d7a9cb2e14
1f10230234 <s1f102302348@iniad.org>
2023-08-20 13:27:33 +0900

    fix typeerror

--- server/api/player/controller.ts
+++ server/api/player/controller.ts
@@ -2,6 +2,5 @@ import { playerUseCase } from '$/useCase/playerUseCase';
 import { defineController } from './$relay';
 
 export default defineController(() => ({
-  get: async () => ({ status: 200, body: await playerUseCase.findAll() }),
   post: async ({ body }) => ({ status: 201, body: await playerUseCase.move(body) }),
 }));
