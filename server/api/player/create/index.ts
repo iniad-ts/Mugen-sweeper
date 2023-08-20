9e04e6854a92f49668fe417d6db34bd678c11fbe
1f10230234 <s1f102302348@iniad.org>
2023-08-21 00:35:41 +0900

    fix typeerror

--- server/api/player/create/index.ts
+++ server/api/player/create/index.ts
@@ -1,10 +1,12 @@
 import type { DefineMethods } from 'aspida';
+import type { UserId } from '../../../commonTypesWithClient/branded';
 import type { PlayerModel } from './../../../commonTypesWithClient/models';
 
 export type Methods = DefineMethods<{
   post: {
     reqBody: {
       name: string;
+      userId: UserId;
     };
     resBody: PlayerModel | null;
   };
