93e655ebb47e59d49c3984b5bc87e201d06bcd0a
1f10230234 <s1f102302348@iniad.org>
2023-08-21 20:44:09 +0900

    add フロントの変更がDBに反映されるよう

--- server/useCase/cellUseCase.ts
+++ server/useCase/cellUseCase.ts
@@ -1,5 +1,6 @@
 import type { UserId } from '$/commonTypesWithClient/branded';
 import { cellsRepository } from '$/repository/cellsRepository';
+import { deepCopy } from '$/service/deepCopy';
 import { sliceWithTime } from '$/service/sliceWithTime';
 
 export const cellUseCase = {
@@ -11,5 +12,14 @@ export const cellUseCase = {
     if (res === null) return;
     const moreOldCells = sliceWithTime(new Date().getTime() - 1000000, res);
     moreOldCells.forEach((cell) => cellsRepository.delete(cell.x, cell.y, cell.whoOpened));
+  },
+  updateUserInputs: async (userInputs: (0 | 1 | 2)[][]) => {
+    const res = await cellsRepository.findAllUserInputted();
+    const newUserInputs = deepCopy<(0 | 1 | 2)[][]>(userInputs);
+    res?.forEach((cell) => {
+      newUserInputs[cell.y][cell.x] = 1;
+      console.log(cell.x, cell.y);
+    });
+    return newUserInputs;
   },
 };
