71e1f6faee1bd86ca939f4adef36364d1e819b40
1f10230234 <s1f102302348@iniad.org>
2023-08-21 20:44:41 +0900

    fix 軽微な変更

--- server/repository/cellsRepository.ts
+++ server/repository/cellsRepository.ts
@@ -5,7 +5,7 @@ import { prismaClient } from '$/service/prismaClient';
 import type { Cell } from '@prisma/client';
 import { z } from 'zod';
 const toCellModel = (prismaCell: Cell): CellModel => ({
-  x: z.number().min(0).parse(prismaCell),
+  x: z.number().min(0).parse(prismaCell.x),
   y: z.number().min(0).parse(prismaCell.y),
   cellValue: z.number().min(0).parse(prismaCell.cellValue),
   whoOpened: userIdParser.parse(prismaCell.whoOpened),
@@ -14,12 +14,17 @@ const toCellModel = (prismaCell: Cell): CellModel => ({
 });
 
 export const cellsRepository = {
-  create: async (cell: CellModel): Promise<CellModel> => {
-    const newCell = { ...cell, whenOpened: new Date(cell.whenOpened) };
-    const prismaCell = await prismaClient.cell.create({
-      data: newCell,
-    });
-    return toCellModel(prismaCell);
+  create: async (cell: CellModel): Promise<CellModel | null> => {
+    try {
+      const newCell: Cell = { ...cell, whenOpened: new Date(cell.whenOpened) };
+      const prismaCell = await prismaClient.cell.create({
+        data: newCell,
+      });
+      return toCellModel(prismaCell);
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
   },
   findAll: async (): Promise<CellModel[]> => {
     const prismaCells = await prismaClient.cell.findMany({
