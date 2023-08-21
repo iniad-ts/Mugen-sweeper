87a91e74a6569564e2b05427bd428ade9f7c7c7a
1f10230234 <s1f102302348@iniad.org>
2023-08-22 07:40:13 +0900

    fix MEMO化

--- client/src/components/GameDisplay/GameDisplay.tsx
+++ client/src/components/GameDisplay/GameDisplay.tsx
@@ -1,55 +1,58 @@
 import type { PlayerModel } from 'commonTypesWithClient/models';
-import { useEffect, useState } from 'react';
-import type { BoardModel, Pos } from 'src/pages/game/index.page';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import type { BoardModel } from 'src/types/types';
 import styles from './GameDisplay.module.css';
 
 const GameDisplay = ({ player, board }: { player: PlayerModel; board: BoardModel }) => {
-  const [currentPlayerPos, setPlayerPos] = useState<Pos>();
+  const [currentPlayerPos, setPlayerPos] = useState<number[]>();
 
   useEffect(() => {
-    setPlayerPos({ x: player.x, y: player.y });
+    setPlayerPos([player.x, player.y]);
   }, [player]);
 
-  const displayPos = (): Pos | undefined => {
+  const displayPos = useCallback((): number[] => {
     if (board[player.y][player.x] === -1) {
-      return currentPlayerPos;
+      return currentPlayerPos ?? [player.x, player.y];
     }
-    return { x: player.x, y: player.y };
-  };
+    return [player.x, player.y];
+  }, [player.x, player.y, board, currentPlayerPos]);
 
-  return (
-    <div className={styles.container}>
-      <div
-        className={styles.display}
-        style={{ gridTemplate: `repeat(${board.length}, 1fr) / repeat(${board[0].length}, 1fr)` }}
-      >
-        {board.map((row, y) =>
-          row.map((val, x) => (
-            <div
-              className={
-                val === -1
-                  ? styles.stone
-                  : [val === 9, val === 10].some(Boolean)
-                  ? `${styles.stone} ${styles.number}`
-                  : styles.number
-              }
-              key={`${y}-${x}`}
-              style={{
-                backgroundPositionX: `${7.65 * (val - 1)}%`,
-                /* stylelint-disable-next-line function-no-unknown */
-                backgroundColor: [val === 9, val === 10, val === -1].some(Boolean)
-                  ? '#4a2'
-                  : '#dca',
-              }}
-            >
-              {[displayPos() !== undefined, displayPos()?.x === x, displayPos()?.y === y].every(
-                Boolean
-              ) && <div className={styles.player} />}
-            </div>
-          ))
-        )}
+  return useMemo(
+    () => (
+      <div className={styles.container}>
+        <div
+          className={styles.display}
+          style={{ gridTemplate: `repeat(${board.length}, 1fr) / repeat(${board[0].length}, 1fr)` }}
+        >
+          {board.map((row, y) =>
+            row.map((val, x) => (
+              <div
+                className={
+                  val === -1
+                    ? styles.stone
+                    : [val === 9, val === 10].some(Boolean)
+                    ? `${styles.stone} ${styles.number}`
+                    : styles.number
+                }
+                key={`${y}-${x}`}
+                style={{
+                  backgroundPositionX: `${7.65 * (val - 1)}%`,
+                  /* stylelint-disable-next-line function-no-unknown */
+                  backgroundColor: [val === 9, val === 10, val === -1].some(Boolean)
+                    ? '#4a2'
+                    : '#dca',
+                }}
+              >
+                {[displayPos() !== undefined, displayPos()[0] === x, displayPos()[1] === y].every(
+                  Boolean
+                ) && <div className={styles.player} />}
+              </div>
+            ))
+          )}
+        </div>
       </div>
-    </div>
+    ),
+    [board, displayPos]
   );
 };
 
