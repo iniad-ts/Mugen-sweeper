474b25ad2f0812aa03faa1a3c9ff8ef9cd658211
1f10230234 <s1f102302348@iniad.org>
2023-08-23 00:23:49 +0900

    add monitorの全player情報表示機能とBAN機能

--- client/src/pages/monitor/index.page.tsx
+++ client/src/pages/monitor/index.page.tsx
@@ -5,15 +5,46 @@ import { apiClient } from 'src/utils/apiClient';
 import { minesweeperUtils } from 'src/utils/minesweeperUtils';
 import type { BoardModel } from '../game/index.page';
 import styles from './index.module.css';
+
+const InfoCell = ({
+  thisPlayer,
+  onClick,
+  val,
+}: {
+  thisPlayer: PlayerModel | undefined;
+  onClick: (value: PlayerModel | undefined) => void;
+  val: number;
+}) => (
+  <div
+    className={styles.cell}
+    style={{ backgroundColor: val === -1 ? '#000' : '#fff' }}
+    onClick={() => onClick(thisPlayer)}
+  >
+    {thisPlayer !== undefined && (
+      <div
+        className={styles.player}
+        style={{ backgroundColor: thisPlayer.isLive ? '0ff8' : '#f008' }}
+      >
+        <div className={styles.playerInfo}>
+          <p>id:{thisPlayer.id}</p>
+          <p>name:{thisPlayer.name}</p>
+          <p>score:{thisPlayer.score}</p>
+          <p>{thisPlayer.isLive ? 'alive' : 'dead'}</p>
+        </div>
+      </div>
+    )}
+  </div>
+);
+
 const Monitor = () => {
-  const [players, setPlayers] = useState<PlayerModel[]>();
-  const [focusedPlayer, refocusedPlayer] = useState<PlayerModel>();
+  const [players, setPlayers] = useState<PlayerModel[]>([]);
+  const [focusedPlayer, setFocusedPlayer] = useState<PlayerModel>();
   const [board, setBoard] = useState<BoardModel>();
 
   const fetchMonitor = async () => {
     const resPlayers = await apiClient.player.$get();
     const resGame = await apiClient.game.$get();
-    if (resPlayers === null || resGame === null) return;
+    if (resGame === null) return;
     const newBoard = minesweeperUtils.makeBoard(resGame.bombMap, resGame.userInputs);
     setBoard(newBoard);
     setPlayers(resPlayers);
@@ -29,6 +60,22 @@ const Monitor = () => {
   if (board === undefined) return <Loading visible />;
   return (
     <div className={styles.container}>
+      <div className={styles['focus-info']}>
+        <div className={styles['focus-info-row']}>{focusedPlayer?.id}</div>
+        <div className={styles['focus-info-row']}>{focusedPlayer?.name}</div>
+        <div className={styles['focus-info-row']}>{focusedPlayer?.score}</div>
+        <div className={styles['focus-info-row']}>{focusedPlayer?.isLive}</div>
+        <button
+          className={styles.button}
+          onClick={async () =>
+            confirm('Are You Sure You Want To Ban This Player?') &&
+            focusedPlayer !== undefined &&
+            (await apiClient.player.delete({ body: focusedPlayer }))
+          }
+        >
+          delete
+        </button>
+      </div>
       <div
         className={styles.main}
         style={{
@@ -38,24 +85,14 @@ const Monitor = () => {
       >
         {board?.map((row, y) =>
           row.map((val, x) => {
-            const thisPlayer = (players ?? []).find((player) => player.x === x && player.y === y);
+            const thisPlayer = players.find((player) => player.x === x && player.y === y);
             return (
-              <div
-                className={styles.cell}
-                style={{ backgroundColor: val === -1 ? '#000' : '#fff' }}
+              <InfoCell
+                thisPlayer={thisPlayer}
+                onClick={setFocusedPlayer}
+                val={val}
                 key={`${y}-${x}`}
-              >
-                {thisPlayer !== undefined && (
-                  <div className={styles.player}>
-                    <div className={styles.playerInfo}>
-                      <p>id:{thisPlayer.id}</p>
-                      <p>name:{thisPlayer.name}</p>
-                      <p>score:{thisPlayer.score}</p>
-                      <p>{thisPlayer.isLive ? 'alive' : 'dead'}</p>
-                    </div>
-                  </div>
-                )}
-              </div>
+              />
             );
           })
         )}
