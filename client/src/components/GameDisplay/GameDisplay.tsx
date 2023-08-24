3466e79c5668bf0c3916761884631271e928d9f5
mst-mkt <s1f102303057@iniad.org>
2023-08-25 06:29:49 +0900

    style: コントローラーのcss等の調整

--- client/src/components/GameDisplay/GameDisplay.tsx
+++ client/src/components/GameDisplay/GameDisplay.tsx
@@ -29,34 +29,32 @@ const GameDisplay = ({ player, board }: { player: PlayerModel; board: BoardModel
   }, [player.x, player.y, board]);
   return useMemo(
     () => (
-      <div className={styles.container}>
-        <div
-          className={styles.display}
-          style={{ gridTemplate: `repeat(${board.length}, 1fr) / repeat(${board[0].length}, 1fr)` }}
-        >
-          {board.map((row, y) =>
-            row.map((val, x) => (
-              <div
-                className={cellStyler(val)}
-                key={`${y}-${x}`}
-                style={{
-                  backgroundPositionX: `${7.65 * (val - 1)}%`,
-                  /* stylelint-disable-next-line  */
-                  backgroundColor: cellBackgroundColor(val),
-                }}
-              >
-                {displayPos !== undefined &&
-                  [displayPos[0] === x, displayPos[1] === y].every(Boolean) && (
-                    <div className={styles.player} />
-                  )}
-                {playerPos !== undefined &&
-                  [playerPos[0] === x, playerPos[1] === y, viewSelectorList.includes(val)].every(
-                    Boolean
-                  ) && <div className={styles.selector} />}
-              </div>
-            ))
-          )}
-        </div>
+      <div
+        className={styles.display}
+        style={{ gridTemplate: `repeat(${board.length}, 1fr) / repeat(${board[0].length}, 1fr)` }}
+      >
+        {board.map((row, y) =>
+          row.map((val, x) => (
+            <div
+              className={cellStyler(val)}
+              key={`${y}-${x}`}
+              style={{
+                backgroundPositionX: `${7.65 * (val - 1)}%`,
+                /* stylelint-disable-next-line  */
+                backgroundColor: cellBackgroundColor(val),
+              }}
+            >
+              {displayPos !== undefined &&
+                [displayPos[0] === x, displayPos[1] === y].every(Boolean) && (
+                  <div className={styles.player} />
+                )}
+              {playerPos !== undefined &&
+                [playerPos[0] === x, playerPos[1] === y, viewSelectorList.includes(val)].every(
+                  Boolean
+                ) && <div className={styles.selector} />}
+            </div>
+          ))
+        )}
       </div>
     ),
     [board, displayPos, playerPos]
--- client/src/pages/controller/Button/index.page.tsx
+++ client/src/pages/controller/Button/index.page.tsx
@@ -2,19 +2,7 @@ import { useMemo } from 'react';
 import type { ActionModel } from 'src/types/types';
 import styles from './index.module.css';
 
-const arrowStyles = {
-  up: `${styles['topRow']} ${styles['middleColumn']}`,
-  left: `${styles['middleRow']} ${styles['leftColumn']}`,
-  right: `${styles['middleRow']} ${styles['rightColumn']}`,
-  down: `${styles['bottomRow']} ${styles['middleColumn']}`,
-  ul: `${styles['topRow']} ${styles['leftColumn']} ${styles.ul}`,
-  ur: `${styles['rightColumn']} ${styles['topRow']} ${styles.ur}`,
-  dl: `${styles['bottomRow']} ${styles['leftColumn']} ${styles.dl}`,
-  dr: `${styles['bottomRow']} ${styles['rightColumn']} ${styles.dr}`,
-};
-
 export const ArrowButton = ({
-  action,
   text,
   onClick,
 }: {
@@ -24,10 +12,10 @@ export const ArrowButton = ({
 }) => {
   return useMemo(
     () => (
-      <button className={`${styles.button} ${arrowStyles[action]}`} onClick={onClick}>
+      <button className={`${styles.button}`} onClick={onClick}>
         {text}
       </button>
     ),
-    [onClick, text, action]
+    [onClick, text]
   );
 };
