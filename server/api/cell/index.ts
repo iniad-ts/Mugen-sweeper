3c0330bc0eac64092645b76575e529c5719d6798
1f10230234 <s1f102302348@iniad.org>
2023-08-24 18:21:30 +0900

    add 仮実装：cellをGETする

--- server/api/cell/index.ts
+++ server/api/cell/index.ts
@@ -1,7 +1,8 @@
+import type { CellModel } from '$/commonTypesWithClient/models';
 import type { DefineMethods } from 'aspida';
 
 export type Methods = DefineMethods<{
   get: {
-    resBody: string;
+    resBody: CellModel[];
   };
 }>;
