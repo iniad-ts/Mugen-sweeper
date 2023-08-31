ea332797b7b7c8232b3865036d455d88773530ee
mst-mkt <s1f102303057@iniad.org>
2023-08-31 23:07:24 +0900

    refactor: コントローラーの軽量化

--- client/src/components/GameDisplay/GameDisplay.tsx
+++ client/src/components/GameDisplay/GameDisplay.tsx
@@ -25,40 +25,43 @@ const GameDisplay = ({ player, board }: { player: PlayerModel; board: BoardModel
   const vmin = useMemo(() => Math.min(windowSize[0], windowSize[1]) / 100, [windowSize]);
 
   return (
-    <div
-      className={styles.display}
-      style={{
-        gridTemplate: `repeat(${board.length}, 1fr) / repeat(${board[0].length}, 1fr)`,
-        top: `${(player.y + 0.7) * vmin * -20 + windowSize[1] / 2}px`,
-        left: `${(player.x + 0.7) * vmin * -20 + windowSize[0] / 2}px`,
-      }}
-    >
-      {board.map((row, y) =>
-        row.map((val, x) => (
-          <div
-            className={styles.cell}
-            key={`${y}-${x}`}
-            style={
-              shouldDisplayNumber.includes(val)
-                ? { backgroundPositionX: `${(val - 1) * -100}%` }
-                : { backgroundImage: 'none' }
-            }
-          >
-            {shouldDisplayStone.includes(val) && (
-              <div
-                className={styles.stone}
-                style={
-                  shouldDisplayStoneImage.includes(val)
-                    ? { backgroundPositionX: `${(val - 1) * -100}%` }
-                    : { backgroundImage: 'none' }
-                }
-              />
-            )}
-          </div>
-        ))
-      )}
+    <>
+      <div
+        className={styles.display}
+        style={{
+          gridTemplate: `repeat(${board.length}, 1fr) / repeat(${board[0].length}, 1fr)`,
+          transform: `translateX(${
+            (player.x + 0.6) * vmin * -20 + windowSize[0] * 0.5
+          }px) translateY(${(player.y + 0.6) * vmin * -20 + windowSize[1] * 0.5}px)`,
+        }}
+      >
+        {board.map((row, y) =>
+          row.map((val, x) => (
+            <div
+              className={styles.cell}
+              key={`${y}-${x}`}
+              style={
+                shouldDisplayNumber.includes(val)
+                  ? { backgroundPositionX: `${(val - 1) * -100}%` }
+                  : { backgroundImage: 'none' }
+              }
+            >
+              {shouldDisplayStone.includes(val) && (
+                <div
+                  className={styles.stone}
+                  style={
+                    shouldDisplayStoneImage.includes(val)
+                      ? { backgroundPositionX: `${(val - 1) * -100}%` }
+                      : { backgroundImage: 'none' }
+                  }
+                />
+              )}
+            </div>
+          ))
+        )}
+      </div>
       <div className={styles.player} />
-    </div>
+    </>
   );
 };
 
